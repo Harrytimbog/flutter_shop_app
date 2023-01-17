28b610afb66518a0915a234a0651ef6e046f9088
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-17 10:50:23 +0100

    Listening in Different Places & Ways

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
@@ -7,9 +9,13 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct = Provider.of<ProductsProvider>(context,
+            listen:
+                false) // listen: true is default. I made it false because I don't want product details screen to always rebuild whenever ProductProvider has a change / updates
+        .findById(productId);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('title')),
+      appBar: AppBar(title: Text(loadedProduct.title)),
     );
   }
 }
