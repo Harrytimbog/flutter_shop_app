b264fb8321dd4451c47637e21a66b5af99104881
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-17 12:29:57 +0100

    Using Nested Models & Providers

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -11,15 +11,18 @@ class ProductsGrid extends StatelessWidget {
     final productsData = Provider.of<ProductsProvider>(context);
     final products = productsData.items;
     return GridView.builder(
-      padding: const EdgeInsets.all(10.0),
-      itemCount: products.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10),
-      itemBuilder: (ctx, i) =>
-          ProductItem(products[i].id, products[i].title, products[i].imageUrl),
-    );
+        padding: const EdgeInsets.all(10.0),
+        itemCount: products.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemBuilder: (ctx, i) => ChangeNotifierProvider(
+              create: (c) => products[i],
+              child: const ProductItem(
+                  // products[i].id, products[i].title, products[i].imageUrl
+                  ),
+            ));
   }
 }
