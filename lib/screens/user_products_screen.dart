75188db69b9d0751c1627fc9fd8cb92b8729f4e4
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-20 11:50:36 +0100

    Implementing Pull-to-Refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shop_app/screens/edit_product_screen.dart';
+import './edit_product_screen.dart';
 import 'package:provider/provider.dart';
 import '../providers/products_provider.dart';
 import '../widgets/user_product_item.dart';
@@ -8,6 +8,11 @@ import '../widgets/app_drawer.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
   const UserProductsScreen({super.key});
+
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<ProductsProvider>(context, listen: false)
+        .fetchAndSetProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,17 +27,20 @@ class UserProductsScreen extends StatelessWidget {
         ),
       ]),
       drawer: const AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (_, i) => Column(
-            children: [
-              UserProductItem(productData.items[i].id!,
-                  productData.items[i].title, productData.items[i].imageUrl),
-              const Divider()
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (_, i) => Column(
+              children: [
+                UserProductItem(productData.items[i].id!,
+                    productData.items[i].title, productData.items[i].imageUrl),
+                const Divider()
+              ],
+            ),
+            itemCount: productData.items.length,
           ),
-          itemCount: productData.items.length,
         ),
       ),
     );
