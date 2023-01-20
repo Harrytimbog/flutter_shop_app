cac05400d45880970dba95076a7033ad6e87b227
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-20 16:44:17 +0100

    Storing Orders in the Web

--- lib/providers/order.dart
+++ lib/providers/order.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import './cart.dart';
 
 class OrderItem {
@@ -21,17 +23,39 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    // insert new orders at the beginning of the list. add method inserts at the end of the list
-    _orders.insert(
-      0,
-      OrderItem(
-        id: DateTime.now().toString(),
-        amount: total,
-        products: cartProducts,
-        dateTime: DateTime.now(),
-      ),
-    );
-    notifyListeners();
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.parse(
+        'https://flutter-store-9c0fe-default-rtdb.europe-west1.firebasedatabase.app/orders.json');
+    final timestamp = DateTime.now();
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': cartProducts
+              .map((cartProduct) => {
+                    'id': cartProduct.id,
+                    'title': cartProduct.title,
+                    'quantity': cartProduct.quantity,
+                    'price': cartProduct.price,
+                  })
+              .toList(),
+        }),
+      );
+      // insert new orders at the beginning of the list. add method inserts at the end of the list
+      _orders.insert(
+        0,
+        OrderItem(
+          id: json.decode(response.body)['name'],
+          amount: total,
+          products: cartProducts,
+          dateTime: timestamp,
+        ),
+      );
+      notifyListeners();
+    } catch (error) {
+      rethrow;
+    }
   }
 }
