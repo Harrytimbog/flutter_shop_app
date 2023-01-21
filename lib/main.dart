20b7a5d53b2a84180e7487939a9526563eb9ad91
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-21 15:35:47 +0100

    Adding User Signup

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products_provider.dart';
 import './providers/order.dart';
+import './providers/auth.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
@@ -22,14 +23,15 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           // Better approach compared to ChangeNotifierProvider.value in this situation (you are newly creating a new object based on a class | since you are not reusing existing objects)
+          create: (ctx) => Auth(),
+        ),
+        ChangeNotifierProvider(
           create: (ctx) => ProductsProvider(),
         ),
         ChangeNotifierProvider(
-          // Better approach compared to ChangeNotifierProvider.value in this situation (you are newly creating a new object based on a class | since you are not reusing existing objects)
           create: (ctx) => Cart(),
         ),
         ChangeNotifierProvider(
-          // Better approach compared to ChangeNotifierProvider.value in this situation (you are newly creating a new object based on a class | since you are not reusing existing objects)
           create: (ctx) => Orders(),
         ),
       ],
---
+++ lib/providers/auth.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  late String _token;
+  late DateTime _expiryDate;
+  late String _userId;
+
+  final apiKey = "AIzaSyAYx3kQ6ktk7sB0bsfp-xGAo03N2cmiLsU";
+
+  Future<void> signup(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey');
+
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+}
