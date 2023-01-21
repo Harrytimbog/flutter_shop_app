3bc632bf1654329bfb1750969dbabb9b924ee030
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-21 15:53:06 +0100

    Allowing Users to Log In

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -9,9 +9,10 @@ class Auth with ChangeNotifier {
 
   final apiKey = "AIzaSyAYx3kQ6ktk7sB0bsfp-xGAo03N2cmiLsU";
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$apiKey');
 
     final response = await http.post(
       url,
@@ -24,5 +25,13 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> signin(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
