01cbffdaa0ac1eb5a54477e613880c9cc8635e42
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-21 11:44:15 +0100

    Using the FutureBuilder Widget & Improving the Code

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,45 +4,46 @@ import '../providers/order.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
   const OrdersScreen({super.key});
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-  @override
-  void initState() {
-    // Future.delayed(Duration.zero).then((_) async {
-    _isLoading = true;
-    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    // });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // print('Building orders'); // to confirm that the whole widget is only built once
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(title: const Text('Your Orders')),
-      drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : (orderData.orders.isEmpty
-              ? const Center(child: Text('You have no order history yet!'))
-              : ListView.builder(
-                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-                  itemCount: orderData.orders.length,
-                )),
-    );
+        appBar: AppBar(title: const Text('Your Orders')),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapShot) {
+            if (dataSnapShot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapShot.error != null) {
+                // ...
+                // Do error handling stuff
+                return const Center(
+                  child: Text('An error occurred!'),
+                );
+              } else {
+                return Consumer<Orders>(
+                  builder: (ctx, orderData, child) => (orderData.orders.isEmpty
+                      ? const Center(
+                          child: Text('You have no order history yet!'))
+                      : ListView.builder(
+                          itemBuilder: (ctx, i) =>
+                              OrderItem(orderData.orders[i]),
+                          itemCount: orderData.orders.length,
+                        )),
+                );
+              }
+            }
+          },
+        ));
   }
 }
