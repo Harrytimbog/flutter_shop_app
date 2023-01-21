48b45d0bf22e078c4c5931977c6ea478e1e6d467
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-21 11:49:18 +0100

    This is a better practice in case you have a reason in the previous method that might cause that widget to rebuild which we want to avoid at all cost

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,9 +4,26 @@ import '../providers/order.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
   const OrdersScreen({super.key});
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  late Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +33,7 @@ class OrdersScreen extends StatelessWidget {
         appBar: AppBar(title: const Text('Your Orders')),
         drawer: const AppDrawer(),
         body: FutureBuilder(
-          future:
-              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          future: _ordersFuture,
           builder: (ctx, dataSnapShot) {
             if (dataSnapShot.connectionState == ConnectionState.waiting) {
               return const Center(
